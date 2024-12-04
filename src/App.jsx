07e1b05b596e6fa724b659e6e366b1f6a3ca1a2f
samirdumre <samirdumre234@gmail.com>
2024-12-04 16:38:45 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
+import PageNotFound from "./pages/PageNotFound";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="Product" element={<Product />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,5 @@
+function PageNotFound() {
+  return <h2>Page not Found</h2>;
+}
+
+export default PageNotFound;
