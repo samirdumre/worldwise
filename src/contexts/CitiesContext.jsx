e5a55bcd46fde0568860ddfa878f574c066ad86b
samirdumre <samirdumre234@gmail.com>
2024-12-10 13:55:57 +0545

    Context API added

---
+++ src/contexts/CitiesContext.jsx
@@ -0,0 +1,38 @@
+import { createContext, useState, useEffect, useContext } from "react";
+
+const CitiesContext = createContext();
+const BASE_URL = "http://localhost:9000";
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        console.log("Error fetching data!");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider value={{ cities, isLoading }}>
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+function useCities() {
+    const context = useContext(CitiesContext);
+    return context;  
+}
+
+export {CitiesProvider, useCities}
